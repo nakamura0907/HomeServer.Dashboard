7e78532ae3af62605ba7158557a9cdd464a88bf9
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2024-08-11 00:01:19 +0900

    echoパッケージ導入

--- core/cmd/main.go
+++ core/cmd/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"embed"
+	"net/http"
 
-	"github.com/nakamura0907/homeserver.dashboard/common"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+// TODO: core/cmd/outなら動作するもののディレクトリ構造を見直す
+//
+//go:embed all:out
+var webAssets embed.FS
+
 func main() {
-	fmt.Println(common.Hello())
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		HTML5:      true,
+		Root:       "out",
+		Browse:     false,
+		Filesystem: http.FS(webAssets),
+	}))
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
