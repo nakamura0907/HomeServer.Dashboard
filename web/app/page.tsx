a4794d784817ce5393277252910db382ca6cdf46
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2024-08-13 09:19:53 +0900

    フォーム部分の作成

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+"use client";
+import { FormEvent, useState } from "react";
 
 type DashboardItem = {
   title: string;
   url: string;
 };
 
-type FormValues = Partial<DashboardItem>;
+type FormValues = DashboardItem;
 
 type State = {
   list: DashboardItem[];
@@ -14,16 +15,68 @@ type State = {
 
 const initialState: State = {
   list: [],
-  formValues: {},
+  formValues: {
+    title: "",
+    url: "",
+  },
 };
 
 export default function Home() {
   const [list, setList] = useState(initialState.list);
   const [formValues, setFormValues] = useState(initialState.formValues);
 
+  const handleChangeFormValue = <T extends keyof FormValues>(
+    key: T,
+    value: FormValues[T]
+  ) => {
+    setFormValues({
+      ...formValues,
+      [key]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formValues);
+
+    // バリデーション
+    const { title, url } = formValues;
+    if (!title || !url) return;
+
+    // リストの更新
+    setList([{ title, url }, ...list]);
+
+    // 終了処理
+    setFormValues(initialState.formValues);
+  };
+
   return (
     <main>
-      <div>{/* form */}</div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">TITLE</label>
+            <input
+              type="text"
+              name="title"
+              value={formValues.title}
+              onChange={(e) => handleChangeFormValue("title", e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              name="url"
+              value={formValues.url}
+              onChange={(e) => handleChangeFormValue("url", e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">SUBMIT</button>
+          </div>
+        </form>
+      </div>
       <div>
         <ul>
           {list.map((item, index) => {
